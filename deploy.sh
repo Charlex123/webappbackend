4f13ddfbd4376dabf964c73def9f505182642369
Charles Muoka <charlesmuoka1@gmail.com>
2024-07-28 09:51:00 +0100

    another commit

--- deploy.sh
+++ deploy.sh
@@ -35,19 +35,46 @@ fi
 
 # Configure Nginx to act as a reverse proxy if not already configured
 if [ ! -f /etc/nginx/conf.d/flaskapp.conf ]; then
-    sudo rm -f $NGINX_CONF
     sudo bash -c "cat > $NGINX_CONF <<EOF
-server {
-    listen 80;
-    server_name $DOMAIN;
+user nginx;
+worker_processes auto;
+error_log /var/log/nginx/error.log;
+pid /run/nginx.pid;
 
-    location / {
-        proxy_pass http://unix:$APP_DIR/flaskapp.sock;
-        proxy_http_version 1.1;
-        proxy_set_header Upgrade \\\$http_upgrade;
-        proxy_set_header Connection 'upgrade';
-        proxy_set_header Host \\\$host;
-        proxy_cache_bypass \\\$http_upgrade;
+events {
+    worker_connections 1024;
+}
+
+http {
+    log_format  main  '\$remote_addr - \$remote_user [\$time_local] "\$request" '
+                      '\$status \$body_bytes_sent "\$http_referer" '
+                      '"\$http_user_agent" "\$http_x_forwarded_for"';
+
+    access_log  /var/log/nginx/access.log  main;
+
+    sendfile            on;
+    tcp_nopush          on;
+    tcp_nodelay         on;
+    keepalive_timeout   65;
+    types_hash_max_size 2048;
+
+    include             /etc/nginx/mime.types;
+    default_type        application/octet-stream;
+
+    include /etc/nginx/conf.d/*.conf;
+
+    server {
+        listen 80;
+        server_name $DOMAIN;
+
+        location / {
+            proxy_pass http://unix:$APP_DIR/flaskapp.sock;
+            proxy_http_version 1.1;
+            proxy_set_header Upgrade \$http_upgrade;
+            proxy_set_header Connection 'upgrade';
+            proxy_set_header Host \$host;
+            proxy_cache_bypass \$http_upgrade;
+        }
     }
 }
 EOF"
