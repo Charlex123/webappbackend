ee0f82dff59ace9265e977cf60460b73c6b916e8
Charles Muoka <charlesmuoka1@gmail.com>
2024-07-30 21:02:28 +0100

    another commit

--- deploy.sh
+++ deploy.sh
@@ -53,75 +53,75 @@ if sudo lsof -i :80 > /dev/null; then
     sudo kill $(sudo lsof -t -i :80)
 fi
 
-NGINX_CONF="/etc/nginx/nginx.conf"
-echo "Creating Nginx reverse proxy configuration"
-sudo bash -c "cat > ${NGINX_CONF} <<EOF
-user nginx;
-worker_processes auto;
-error_log /var/log/nginx/error.log notice;
-pid /run/nginx.pid;
+# NGINX_CONF="/etc/nginx/nginx.conf"
+# echo "Creating Nginx reverse proxy configuration"
+# sudo bash -c "cat > ${NGINX_CONF} <<EOF
+# user nginx;
+# worker_processes auto;
+# error_log /var/log/nginx/error.log notice;
+# pid /run/nginx.pid;
 
-include /usr/share/nginx/modules/*.conf;
+# include /usr/share/nginx/modules/*.conf;
 
-events {
-    worker_connections 1024;
-}
+# events {
+#     worker_connections 1024;
+# }
 
-http {
-    log_format  main  '$remote_addr - $remote_user [$time_local] "$request" '
-                      '$status $body_bytes_sent "$http_referer" '
-                      '"$http_user_agent" "$http_x_forwarded_for"';
+# http {
+#     log_format  main  '$remote_addr - $remote_user [$time_local] "$request" '
+#                       '$status $body_bytes_sent "$http_referer" '
+#                       '"$http_user_agent" "$http_x_forwarded_for"';
                       
-    access_log  /var/log/nginx/access.log  main;
+#     access_log  /var/log/nginx/access.log  main;
 
-    sendfile            on;
-    tcp_nopush          on;
-    tcp_nodelay         on;
-    keepalive_timeout   65;
-    types_hash_max_size 2048;
+#     sendfile            on;
+#     tcp_nopush          on;
+#     tcp_nodelay         on;
+#     keepalive_timeout   65;
+#     types_hash_max_size 2048;
 
-    include             /etc/nginx/mime.types;
-    default_type        application/octet-stream;
+#     include             /etc/nginx/mime.types;
+#     default_type        application/octet-stream;
 
-    server {
-        listen 80;
-        server_name webappbackend.fifareward.io www.webappbackend.fifareward.io;
+#     server {
+#         listen 80;
+#         server_name webappbackend.fifareward.io www.webappbackend.fifareward.io;
 
-        location / {
-            proxy_pass http://127.0.0.1:8080;
-            proxy_set_header Host \$host;
-            proxy_set_header X-Real-IP \$remote_addr;
-            proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
-            proxy_set_header X-Forwarded-Proto \$scheme;
-        }
+#         location / {
+#             proxy_pass http://127.0.0.1:8080;
+#             proxy_set_header Host \$host;
+#             proxy_set_header X-Real-IP \$remote_addr;
+#             proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
+#             proxy_set_header X-Forwarded-Proto \$scheme;
+#         }
 
-        access_log /var/log/nginx/webappbackend.fifareward.io.access_log;
-        error_log /var/log/nginx/webappbackend.fifareward.io.error_log;
+#         access_log /var/log/nginx/webappbackend.fifareward.io.access_log;
+#         error_log /var/log/nginx/webappbackend.fifareward.io.error_log;
 
-        listen [::]:443 ssl ipv6only=on; # managed by Certbot
-        listen 443 ssl; # managed by Certbot
-        ssl_certificate /etc/letsencrypt/live/webappbackend.fifareward.io/fullchain.pem; # managed by Certbot
-        ssl_certificate_key /etc/letsencrypt/live/webappbackend.fifareward.io/privkey.pem; # managed by Certbot
-        include /etc/letsencrypt/options-ssl-nginx.conf; # managed by Certbot
-        ssl_dhparam /etc/letsencrypt/ssl-dhparams.pem; # managed by Certbot
-    }
+#         listen [::]:443 ssl ipv6only=on; # managed by Certbot
+#         listen 443 ssl; # managed by Certbot
+#         ssl_certificate /etc/letsencrypt/live/webappbackend.fifareward.io/fullchain.pem; # managed by Certbot
+#         ssl_certificate_key /etc/letsencrypt/live/webappbackend.fifareward.io/privkey.pem; # managed by Certbot
+#         include /etc/letsencrypt/options-ssl-nginx.conf; # managed by Certbot
+#         ssl_dhparam /etc/letsencrypt/ssl-dhparams.pem; # managed by Certbot
+#     }
 
-    server {
-        if (\$host = www.webappbackend.fifareward.io) {
-            return 301 https://\$host\$request_uri;
-        } # managed by Certbot
+#     server {
+#         if (\$host = www.webappbackend.fifareward.io) {
+#             return 301 https://\$host\$request_uri;
+#         } # managed by Certbot
 
-        if (\$host = webappbackend.fifareward.io) {
-            return 301 https://\$host\$request_uri;
-        } # managed by Certbot
+#         if (\$host = webappbackend.fifareward.io) {
+#             return 301 https://\$host\$request_uri;
+#         } # managed by Certbot
 
-        listen 80;
-        listen [::]:80;
-        server_name webappbackend.fifareward.io www.webappbackend.fifareward.io;
-        return 404; # managed by Certbot
-    }
-}
-EOF"
+#         listen 80;
+#         listen [::]:80;
+#         server_name webappbackend.fifareward.io www.webappbackend.fifareward.io;
+#         return 404; # managed by Certbot
+#     }
+# }
+# EOF"
 
 echo "Testing Nginx configuration"
 sudo nginx -t
