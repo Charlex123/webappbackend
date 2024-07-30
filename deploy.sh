c5795a5d93935c1026317d24e99c8af681db8311
Charles Muoka <charlesmuoka1@gmail.com>
2024-07-30 19:07:01 +0100

    another commit

--- deploy.sh
+++ deploy.sh
@@ -53,71 +53,77 @@ if sudo lsof -i :80 > /dev/null; then
     sudo kill $(sudo lsof -t -i :80)
 fi
 
-# NGINX_CONF="/etc/nginx/nginx.conf"
-# echo "Creating Nginx reverse proxy configuration"
-# sudo bash -c "cat > ${NGINX_CONF} <<EOF
-# user nginx;
-# worker_processes auto;
-# error_log /var/log/nginx/error.log;
-# pid /run/nginx.pid;
+NGINX_CONF="/etc/nginx/nginx.conf"
+echo "Creating Nginx reverse proxy configuration"
+sudo bash -c "cat > ${NGINX_CONF} <<EOF
+user nginx;
+worker_processes auto;
+error_log /var/log/nginx/error.log;
+pid /run/nginx.pid;
 
-# events {
-#     worker_connections 1024;
-# }
+events {
+    worker_connections 1024;
+}
 
-# http {
-#     log_format  main  '$remote_addr - $remote_user [$time_local] "$request" '
-#                       '$status $body_bytes_sent "$http_referer" '
-#                       '"$http_user_agent" "$http_x_forwarded_for"';
+http {
+    log_format  main  '$remote_addr - $remote_user [$time_local] "$request" '
+                      '$status $body_bytes_sent "$http_referer" '
+                      '"$http_user_agent" "$http_x_forwarded_for"';
                       
-#     access_log  /var/log/nginx/access.log  main;
+    access_log  /var/log/nginx/access.log  main;
 
-#     sendfile            on;
-#     tcp_nopush          on;
-#     tcp_nodelay         on;
-#     keepalive_timeout   65;
-#     types_hash_max_size 2048;
+    sendfile            on;
+    tcp_nopush          on;
+    tcp_nodelay         on;
+    keepalive_timeout   65;
+    types_hash_max_size 2048;
 
-#     include             /etc/nginx/mime.types;
-#     default_type        application/octet-stream;
+    include             /etc/nginx/mime.types;
+    default_type        application/octet-stream;
 
-#     upstream flaskapp {
-#         server 54.161.105.37:80;
-#     }
+    upstream flaskapp {
+        server 54.161.105.37:80;
+    }
 
-#     server {
-#         listen 80;
-#         server_name webappbackend.fifareward.io;
+    server {
+        listen 80;
+        server_name webappbackend.fifareward.io www.webappbackend.fifareward.io;
 
-#         location / {
-#             proxy_pass http://flaskapp;
-#         }
+        location / {
+            proxy_pass http://flaskapp;
+            proxy_set_header Host \$host;
+            proxy_set_header X-Real-IP \$remote_addr;
+            proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
+            proxy_set_header X-Forwarded-Proto \$scheme;
+        }
 
-#         location ~ /.well-known/acme-challenge {
-#             allow all;
-#         }
-#     }
+        access_log /var/log/nginx/webappbackend.fifareward.io.access_log;
+        error_log /var/log/nginx/webappbackend.fifareward.io.error_log;
 
-#     server {
-#         listen 443 ssl;
-#         server_name webappbackend.fifareward.io;
+        listen [::]:443 ssl ipv6only=on; # managed by Certbot
+        listen 443 ssl; # managed by Certbot
+        ssl_certificate /etc/letsencrypt/live/webappbackend.fifareward.io/fullchain.pem; # managed by Certbot
+        ssl_certificate_key /etc/letsencrypt/live/webappbackend.fifareward.io/privkey.pem; # managed by Certbot
+        include /etc/letsencrypt/options-ssl-nginx.conf; # managed by Certbot
+        ssl_dhparam /etc/letsencrypt/ssl-dhparams.pem; # managed by Certbot
+    }
 
-#         ssl_certificate /etc/letsencrypt/live/webappbackend.fifareward.io/fullchain.pem;
-#         ssl_certificate_key /etc/letsencrypt/live/webappbackend.fifareward.io/privkey.pem;
-#         ssl_protocols TLSv1.2 TLSv1.3;
-#         ssl_prefer_server_ciphers on;
+    server {
+        if (\$host = www.webappbackend.fifareward.io) {
+            return 301 https://\$host\$request_uri;
+        } # managed by Certbot
 
-#         location / {
-#             proxy_pass http://flaskapp;
-#             proxy_set_header Host $host;
-#             proxy_set_header X-Real-IP $remote_addr;
-#             proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
-#             proxy_set_header X-Forwarded-Proto $scheme;
-#         }
-#     }
-# }
+        if (\$host = webappbackend.fifareward.io) {
+            return 301 https://\$host\$request_uri;
+        } # managed by Certbot
 
-# EOF"
+        listen 80;
+        listen [::]:80;
+        server_name webappbackend.fifareward.io www.webappbackend.fifareward.io;
+        return 404; # managed by Certbot
+    }
+}
+EOF"
 
 echo "Testing Nginx configuration"
 sudo nginx -t
