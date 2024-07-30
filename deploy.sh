4678d1dae43686c9158cd647339ff898d4d5e14c
Charles Muoka <charlesmuoka1@gmail.com>
2024-07-30 22:37:00 +0100

    another commit

--- deploy.sh
+++ deploy.sh
@@ -61,10 +61,10 @@ server {
 
     location / {
         proxy_pass http://127.0.0.1:8080;
-        proxy_set_header Host \$host;
-        proxy_set_header X-Real-IP \$remote_addr;
-        proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
-        proxy_set_header X-Forwarded-Proto \$scheme;
+        proxy_set_header Host $host;
+        proxy_set_header X-Real-IP $remote_addr;
+        proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+        proxy_set_header X-Forwarded-Proto $scheme;
     }
 
     access_log /var/log/nginx/access_log;
@@ -79,12 +79,12 @@ server {
 }
 
 server {
-    if (\$host = www.webappbackend.fifareward.io) {
-        return 301 https://\$host\$request_uri;
+    if ($host = www.webappbackend.fifareward.io) {
+        return 301 https://$host$request_uri;
     }
 
-    if (\$host = webappbackend.fifareward.io) {
-        return 301 https://\$host\$request_uri;
+    if ($host = webappbackend.fifareward.io) {
+        return 301 https://$host$request_uri;
     }
 
     listen 80;
