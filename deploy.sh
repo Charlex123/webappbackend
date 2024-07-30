5dace560e6f29546e110a2d77122794462e30b83
Charles Muoka <charlesmuoka1@gmail.com>
2024-07-30 09:17:22 +0100

    another commit

--- deploy.sh
+++ deploy.sh
@@ -16,60 +16,47 @@ sudo mkdir -p $APP_DIR
 echo "Moving files to app folder"
 sudo cp -r $EC2_USER_DIR/* $APP_DIR
 
-# Ensure the app directory has the correct permissions
 echo "Setting ownership of app directory"
 sudo chown -R ec2-user:ec2-user $APP_DIR
 
-# Navigate to the app directory
 cd $APP_DIR
 
-# Create and activate virtual environment
 echo "Setting up virtual environment"
 python3 -m venv venv
 source venv/bin/activate
 
-# Install application dependencies from requirements.txt
 echo "Installing application dependencies from requirements.txt"
 pip install --upgrade pip
 pip install -r requirements.txt
 
-# Uninstall Nginx if it is already installed
 if command -v nginx > /dev/null; then
     echo "Uninstalling Nginx"
     sudo yum remove -y nginx
 fi
 
-# Install Nginx
 echo "Installing Nginx"
 sudo yum install -y nginx
 
-# Stop any existing Gunicorn process
 echo "Stopping any existing Gunicorn process"
 sudo pkill gunicorn || true
 sudo rm -rf flaskapp.sock
 
-# Start Gunicorn with the Flask application using the virtual environment
 echo "Starting Gunicorn"
 sudo $APP_DIR/venv/bin/gunicorn --workers 3 --bind unix:$APP_DIR/flaskapp.sock app:app --daemon
-echo "Started Gunicorn 🚀"
 
-# Stop any existing bot.py process
 if pgrep -f "bot.py" > /dev/null; then
     echo "Stopping existing bot.py process"
     pkill -f "bot.py"
 fi
 
-# Start the bot script using the virtual environment
 echo "Starting bot.py"
 nohup $APP_DIR/venv/bin/python3 bot.py &
 
-# Check if port 80 is in use and stop the process if needed
 if sudo lsof -i :80 > /dev/null; then
     echo "Port 80 is in use, stopping the process"
     sudo kill $(sudo lsof -t -i :80)
 fi
 
-# Create Nginx reverse proxy configuration
 NGINX_CONF="/etc/nginx/conf.d/flaskapp.conf"
 echo "Creating Nginx reverse proxy configuration"
 sudo bash -c "cat > $NGINX_CONF <<EOF
@@ -88,7 +75,6 @@ server {
 }
 EOF"
 
-# Test Nginx configuration before restarting
 echo "Testing Nginx configuration"
 sudo nginx -t
 
@@ -96,11 +82,9 @@ if [ $? -eq 0 ]; then
     echo "Restarting Nginx"
     sudo systemctl restart nginx
 
-    # Verify DNS resolution
     echo "Checking DNS resolution for $DOMAIN"
     if nslookup $DOMAIN; then
         echo "DNS resolution successful, obtaining SSL certificate with Certbot"
-        # Install Certbot and obtain SSL certificate
         if ! command -v certbot > /dev/null; then
             echo "Installing Certbot"
             sudo yum install -y certbot python3-certbot-nginx
