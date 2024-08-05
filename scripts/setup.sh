2ba4f291d87fc1019a8c0e8f6989dae24d8b18fa
Charles Muoka <charlesmuoka1@gmail.com>
2024-08-05 17:01:28 +0100

    another commit

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -6,7 +6,6 @@ python -m venv venv
 source venv/bin/activate
 
 # Install required packages
-pip install upgrade pip
 pip install -r requirements.txt
 
 # Initialize Alembic (ensure your alembic.ini is configured)
