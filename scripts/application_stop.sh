e6a21f5b04cbfa1858d7c3a272b2dbf9bb57d419
Charles Muoka <charlesmuoka1@gmail.com>
2024-08-06 15:17:46 +0100

    another commit

--- scripts/application_stop.sh
+++ scripts/application_stop.sh
@@ -4,6 +4,7 @@ set -xe
 # sudo systemctl stop codedeploy-agent
 # sudo rm -rf /opt/codedeploy-agent/deployment-root
 sudo systemctl start codedeploy-agent
+
 cd /home/ec2-user/webappbackend
 
 docker-compose down
