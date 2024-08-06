d98778cf22eb6cc737fabbbbf8715e2c59a33cf3
Charles Muoka <charlesmuoka1@gmail.com>
2024-08-06 06:09:57 +0100

    another commit

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -13,12 +13,27 @@ sudo rm -rf /home/ec2-user/webappbackend
 
 # Update packages
 sudo yum -y update
-sudo yum -y install ruby
-sudo yum -y install wget
+# sudo yum -y install ruby
+# sudo yum -y install wget
 cd /home/ec2-user
 # wget https://aws-codedeploy-ap-south-1.s3.ap-south-1.amazonaws.com/latest/install
 # sudo chmod +x ./install
 # sudo ./install auto
+# Ensure the ec2-user has ownership of the directory
+sudo chown -R ec2-user:ec2-user /home/ec2-user/webappbackend
+
+# Install Docker if not already installed
+if ! command -v docker &> /dev/null; then
+  sudo yum install -y docker
+  sudo service docker start
+  sudo usermod -a -G docker ec2-user
+fi
+
+# Install Docker Compose if not already installed
+if ! command -v docker-compose &> /dev/null; then
+  sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+  sudo chmod +x /usr/local/bin/docker-compose
+fi
 
 # Install coreutils if nohup is not available
 if ! command -v unzip &> /dev/null
